1f0bbb7b885e249f4910d6d9b856d04eb94c6801
unknown <c6mivoim@KC411B20.tamk.ad>
2018-01-08 12:24:33 +0200

    Initial Unity project

---
+++ Assets/Code/Units/PlayerUnit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame
+{
+    public class PlayerUnit : Unit
+    {
+        protected override void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Code/Units/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame
+{
+    public abstract class Unit : MonoBehaviour
+    {
+        public virtual void Init()
+        {
+
+        }
+
+        public virtual void Clear()
+        {
+
+        }
+
+        // An abstract method has to be defined in a non-abstract child class.
+        protected abstract void Update();
+    }
+}
